dffb290bb3dcdde84b78b1ae2dc73ca8de41ff3a
m8test <github@m8test.com>
2024-03-14 20:43:23 +0800

    12

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 const exec = require("@actions/exec")
+const axios = require("axios");
 
 const setupGit = async () => {
     await exec.exec(`git config --global user.name "gh-automation"`);
@@ -25,10 +26,12 @@ async function run() {
     const debug = core.getBooleanInput("debug")
     const repository = core.getInput("repository", {required: true})
     const logger = setupLogger({debug, prefix: '[release-to-version]'});
-    logger.info(`repository:${repository}`)
-    console.log(core)
-    console.log(github)
-    console.log(exec)
+    logger.info(`仓库:${repository}`)
+    // 请求 url
+    const url = `https://api.github.com/repos/${repository}/releases/latest`
+    let json = (await axios.get(url)).data
+    logger.debug(json)
+    logger.info("运行结束")
 }
 
 run()
