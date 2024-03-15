d208d9dc486cbb7e2263b2fd1b21dba85928a034
m8test <github@m8test.com>
2024-03-15 14:25:32 +0800

    40

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ echo "project dir is $projectDir, debug enabled is $debug"
 
 # shellcheck disable=SC2039
 if [ "$debug" == "true" ]; then
+  echo "run 'npm run debug'"
   npm run debug
 else
+  echo "run 'npm run prod'"
   npm run prod
 fi
