07370938259fa45b60350076c62599fbe054135d
m8test <github@m8test.com>
2024-03-15 14:24:25 +0800

    39

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-echo "hello $*"
+projectDir=$1
+debug=$2
+cd "$projectDir" || exit
+npm install
+echo "project dir is $projectDir, debug enabled is $debug"
+
+# shellcheck disable=SC2039
+if [ "$debug" == "true" ]; then
+  npm run debug
+else
+  npm run prod
+fi
