2ee0dfed7433ad0d0c3930b3562026b55ca1d06b
m8test <github@m8test.com>
2024-03-15 21:15:12 +0800

    45

--- src/release.js
+++ src/release.js
@@ -8,19 +8,21 @@ async function getLatestRelease(repository) {
 }
 
 class VersionInfo {
-    constructor(version_name, body, browser_download_url, filename, update_at) {
+    constructor(version_name, body, browser_download_url, filename, update_at, content_type, size) {
         this.version_name = version_name
         this.body = body
         this.browser_download_url = browser_download_url
         this.filename = filename
         this.update_at = update_at
+        this.content_type = content_type
+        this.size = size
     }
 }
 
 function getVersionInfo(release) {
     let arr = []
     release.assets.forEach((value, index, array) => {
-        arr.push(new VersionInfo(release.version_name, release.body, value.browser_download_url, value.name, value.updated_at))
+        arr.push(new VersionInfo(release.version_name, release.body, value.browser_download_url, value.name, value.updated_at, value.content_type, value.size))
     })
     return arr
 }
