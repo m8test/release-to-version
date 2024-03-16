fbe50d9b358ce64b7fd852d30a3b7c952083a199
m8test <github@m8test.com>
2024-03-16 11:25:41 +0800

    52

--- .github/actions/version/src/index.js
+++ .github/actions/version/src/index.js
@@ -3,6 +3,7 @@ const github = require("@actions/github")
 const exec = require("@actions/exec")
 const {getLatestRelease, getVersionInfo} = require("./release");
 const fs = require("fs");
+const path = require("path");
 
 const setupGit = async () => {
     await exec.exec(`git config --global user.name "github-actions"`);
@@ -26,6 +27,10 @@ const setupLogger = ({debug, prefix} = {debug: false, prefix: ''}) => ({
 async function run() {
     const debug = core.getBooleanInput("debug")
     const repository = core.getInput("repository", {required: true})
+    // 工作路径
+    const workingDirectory = core.getInput("working_directory", {required: true})
+    // 输出路径，和 workingDirectory 组合成完整路径
+    const outputFile = core.getInput("output_file", {required: true})
     const logger = setupLogger({debug, prefix: '[release-to-version]'});
     logger.info(`仓库:${repository}`)
     // 请求仓库 release url
@@ -37,8 +42,21 @@ async function run() {
         logger.debug(JSON.stringify(item))
     })
     await setupGit()
+    const commonExecOpts = {
+        cwd: workingDirectory,
+    };
     // 将版本信息写入文件
-    await exec.exec("ls -alh")
+    fs.writeFileSync(path.join(workingDirectory, outputFile), JSON.stringify(versions))
+    let output = await exec.getExecOutput("git status -s", {...commonExecOpts})
+    if (output.stdout.length > 0) {
+        // 文件更改了
+        logger.info(`发现新版本,开始提交`)
+        await exec.exec("git add .", {...commonExecOpts})
+        await exec.exec(`git commit -m "${data.body}"`, {...commonExecOpts})
+        await exec.exec(`git push"`, {...commonExecOpts})
+    } else {
+        logger.info(`没有发现新版本`)
+    }
     core.setOutput("version", `${data.tag_name}`)
     core.setOutput("update_content", `${data.body}`)
     logger.info("运行结束")
