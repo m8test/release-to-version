0ca920144100e55e41d1db4e7cf180b3383819a6
m8test <github@m8test.com>
2024-03-19 12:23:04 +0800

    1

--- .github/actions/copy/src/github.js
+++
@@ -1,7 +0,0 @@
-const exec = require("@actions/exec")
-
-async function clone(working_directory, GITHUB_TOKEN, GITHUB_REPOSITORY) {
-    await exec.exec(`git clone https://${GITHUB_REPOSITORY.split("/")[0]}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`, [], {cwd: working_directory})
-}
-
-module.exports = {clone: clone}
